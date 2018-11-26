8710794bfab5d9e1d2ef19b7207899af647cb288
Loic Sharma <sharma.loic@gmail.com>
2018-11-25 20:28:43 -0800

    Initial commit

---
+++ LosharTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace loshar_symbols
+{
+    public class LosharTest
+    {
+        public static int Test(int a, int b)
+        {
+            if (a % 2 == 0)
+            {
+                return a + b;
+            }
+            else
+            {
+                return a - b;
+            }
+        }
+    }
+}
